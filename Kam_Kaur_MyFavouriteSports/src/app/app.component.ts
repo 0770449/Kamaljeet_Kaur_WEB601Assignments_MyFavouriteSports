0a34a42075c4ea4ea47e04962ea75af2fbf45a55
Kamaljeet Kaur <jeetkamalkaur303@gmail.com>
2023-02-01 19:00:31 -0500

    Assignment1

---
+++ Kam_Kaur_MyFavouriteSports/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Kam_Kaur_MyFavouriteSports';
+  name: string="Kamal";
+}
+  
