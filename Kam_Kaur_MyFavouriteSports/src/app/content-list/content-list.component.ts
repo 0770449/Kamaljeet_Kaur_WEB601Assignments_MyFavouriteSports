9f9fe772741d052f7973cf52c45a63de272327bd
Kamaljeet Kaur <jeetkamalkaur303@gmail.com>
2023-02-02 22:49:33 -0500

    Assignment 2 competed

--- Kam_Kaur_MyFavouriteSports/src/app/content-list/content-list.component.ts
+++ Kam_Kaur_MyFavouriteSports/src/app/content-list/content-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 // import { ContentList } from '../helper-files/content-list';
 
 import { Content } from '../helper-files/content-interface';
@@ -10,7 +10,7 @@ import { Content } from '../helper-files/content-interface';
 })
 export class ContentListComponent implements OnInit {
 	
-	content :Content[];
+	@Input() content :Content[];
 	
 	ngOnInit () {
 	 
